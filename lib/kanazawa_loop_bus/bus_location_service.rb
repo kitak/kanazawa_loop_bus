5b4d9e2a60230ad6ddd7c81def7f3f40435a33a8
Keisuke KITA <kei.kita2501@gmail.com>
2013-02-13 13:05:32 +0900

    create bus_location_service class

---
+++ lib/kanazawa_loop_bus/bus_location_service.rb
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+module KanazawaLoopBus
+  class BusLocationService
+    attr_reader :fetch_time
+
+    def initialize
+      fetch
+    end
+
+    def fetch
+      @doc = Nokogiri::HTML(fetch_html)
+      raise "Service Finished" if service_finished?
+      @fetch_time = Time.now
+    end
+    alias_method :refetch, :fetch
+
+    def buses
+      @doc.xpath('//td/img').select { |img|
+        /\.\.\/img\/busicon\/car_s_.\.gif/ =~ img["src"]
+      }.map { |img|
+        Bus.from_node(img)
+      }
+    end
+
+    private
+    def fetch_html
+      open('http://vbusloc.hokutetsu.co.jp/bus/?rno=1&lang=jp').read
+                                                               .encode("UTF-8", "Shift_JIS",
+                                                                       invalid: :replace, undef: :replace, replace: "")
+    end
+
+    def service_finished?
+      @doc.css('//td/img').select { |img|
+        /\.\.\/img\/busicon\/car_s_.\.gif/ =~ img["src"]
+      }.empty?
+    end
+  end
+end
