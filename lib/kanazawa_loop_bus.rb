5fc92629b394260c6be5df66f561201ffa06d17a
Keisuke KITA <kei.kita2501@gmail.com>
2013-02-13 13:14:26 +0900

    write sample

--- lib/kanazawa_loop_bus.rb
+++ lib/kanazawa_loop_bus.rb
@@ -5,3 +5,10 @@ require "kanazawa_loop_bus/bus_location_service"
 
 module KanazawaLoopBus
 end
+
+if __FILE__ == $0
+  service = KanazawaLoopBus::BusLocationService.new
+  puts service.fetch_time
+  puts service.buses[0].name
+  puts service.buses[0].next_stop.name
+end
